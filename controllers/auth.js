77a56965c3e3b8f8d287f97486ce747883d83aa6
Memo Infante <jose.inmo97dev@outlook.com>
2022-02-28 17:06:54 -0600

    Google Sign-In front and back done

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const bcryptjs = require("bcryptjs");
 const User = require('../models/user');
 
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async(req, res = response) =>{
     const {eMail, password} = req.body;
@@ -47,6 +48,51 @@ const login = async(req, res = response) =>{
     }
 }
 
+const googleSignIn = async(req, res = response) => {
+    const { id_token } = req.body;
+
+    try{
+        const {name, eMail, image} = await googleVerify(id_token);
+        
+        let usuario = await User.findOne({eMail});
+
+        if(!usuario){
+            //Crear usuario
+            const data = {
+                name,
+                eMail,
+                password: ':P',
+                image,
+                google: true
+            };
+            usuario = new User(data);
+            await usuario.save();
+            console.log('Se ha guardado nuevo usuario', usuario)
+        }
+
+        //Si el usuario ya se encuentra en DB
+        if(!usuario.active){
+            return res.status(401),json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        });
+    }catch(error){
+        res.status(400).json({
+            ok: false,
+            msg: 'El Token no se pudo verificar'
+        });
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
